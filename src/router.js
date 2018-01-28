444f988fa9a299f9f1ddd3eccc4e09b31674a167
macbookpro <xypisces@163.com>
2018-01-28 18:09:15 +0800

    update

--- src/router.js
+++ src/router.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Router, Route, Switch } from 'dva/router';
 import IndexPage from './routes/IndexPage';
+import List from './routes/home/List';
 
 function RouterConfig({ history }) {
   return (
     <Router history={history}>
       <Switch>
-        <Route path="/" exact component={IndexPage} />
+        <Route path="/" exact component={List} />
+        <Route path="/index" exact component={IndexPage} />
       </Switch>
     </Router>
   );
